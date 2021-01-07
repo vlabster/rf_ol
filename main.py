8d5dc41a33c3c34c4f641923c90fab66d5786fad
vlabster <vlabster@mail.ru>
2021-01-07 15:38:07 +0300

    added font, text to img

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 data = ["name", "surname", "burthDate", "Country", "Obl", "date1", "date2", "data3", "country2", "obl2", "categories"]
 
 img1 = Image.open('sample.png')
 img2 = Image.open('photos/1.png')
 
 img1.paste(img2, (70, 170))
+
+finImg = ImageDraw.Draw(img1)
+
+
+# set font
+font = ImageFont.truetype('fonts/20051.ttf', size=50)
+
+# draw text
+finImg.text(
+    (500, 150),
+    'Name',
+    font=font,
+    fill='#1C0606'
+)
+
 img1.save('result.png')
