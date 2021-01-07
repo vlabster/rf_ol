9e8e1dac0d2724ef5a1267a9b0733c5c64177a4a
vasemkin <vasemkin@ya.ru>
2021-01-07 16:51:29 +0300

    separate files

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from random import randint
-# data = ["name", "surname", "burthDate", "Country", "Obl", "date1", "date2", "data3", "country2", "obl2", "categories"]
+from random import randint, choice
+import os
 from PIL import Image, ImageFont, ImageDraw
+
 data = ["name", "surname", "burthDate", "Country", "Obl", "date1", "date2", "data3", "country2", "obl2", "categories"]
+
 
 img1 = Image.open('sample.png')
 img2 = Image.open('photos/1.png')
@@ -34,22 +36,3 @@ img1.paste(img2, (70, 170))
 
 
 
-canvas = Image.new(mode = "RGB", size = (900,600))
-
-### working with the main canvas backround
-
-bg = Image.open('bgs/0.jpg')
-
-def random_bg_crop():
-    left = randint(0,200)
-    upper = randint(0,200)
-    right = left + 900
-    bottom = upper + 600
-    return((left, upper, right, bottom))
-
-newbg = bg.crop(random_bg_crop())
-
-
-
-canvas.paste(newbg, (0,0))
-canvas.save('canvas.png')
