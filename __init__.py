71ce0e3c8b6a0975b80455bb2fbad915c2c4d4c8
vasemkin <57621898+vasemkin@users.noreply.github.com>
2021-10-25 17:12:40 +0300

    trying to make the flag the same

--- __init__.py
+++ __init__.py
@@ -493,6 +493,12 @@ def generate_random_id_test(first_name = "RANDOM", middle_name = "RANDOM", secon
         countries = open(str(here) + '/resourses/countries.txt')
         country = random_line(countries).upper()
 
+    def find_files(filename, search_path):
+        filename = filename.split(' ')[0].lower()
+        result = []
+        for root, dirs, files in os.walk(search_path):
+            result.append(list(filter(lambda name: filename in name, files)))
+        return result[0]
 
     def get_random_date(start_year, end_year):
         start_date = datetime.date(start_year, 1, 1)
@@ -644,7 +650,10 @@ def generate_random_id_test(first_name = "RANDOM", middle_name = "RANDOM", secon
 
     ### adding the static elements
     if (flags == "RANDOM"):
-        randpath_flag = choice(os.listdir(str(here) + '/flags'))
+        if (len(find_files(country, str(here) + '/flags/')) > 0):
+            randpath_flag = find_files(country, str(here) + '/flags/')[0]
+        else:
+            randpath_flag = choice(os.listdir(str(here) + '/flags'))
         flag = Image.open(str(here) + '/flags/' + randpath_flag)
     else:
         flag = Image.open(str(here) + '/resourses/04.png').convert("RGBA")
